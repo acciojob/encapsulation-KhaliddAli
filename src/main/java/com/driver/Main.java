b03c520edb59074b4b920a9d098fec9606d7f9f3
khalid <khalidali967@gmail.com>
2023-09-21 13:08:32 +0530

    first attempt

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+        //obj.name = "Khalid";
+        //error - The field RWOnly.name is not visibleJava(33554503)
+
+        obj.setName("Khalid");
+        System.out.println(obj.getName());
+    }
+
 }
